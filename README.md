917652bfbe86dffc4a43b77efc7f39fbca8004ba
Welton Rodrigo <rodrigo@familianascimento.org>
2014-06-24 02:31:18 -0400

    Initial import

---
+++ README.md
@@ -0,0 +1,48 @@
+importio-signer-heroku
+======================
+
+A simple node.js app, deployable to Heroku, for signing import.io queries for your zumbi clients out there.
+
+## Rationale
+
+If you use Import.IO for scraping (and you should!), you must use a signing server for signing(!) your clients queries. In other words, its a _bad idea_ to let your api key lay open on the source of your super index.html or even on a hyper uglyfied javascript compiled from typescript compiled from Visual Basic 2013 Web Express or something like that.
+
+That being said, you should:
+
+## Step-by-step
+
+```bash
+git clone https://github.com/weltonrodrigo/importio-signer-heroku.git
+cd importio-signer-heroku
+heroku login
+heroku create
+heroku push master
+
+heroku config:set IMPORTIO_USERGUID=21fe0000-0000-0000-0000-af4300007640
+heroku config:set IMPORTIO_APIKEY=your_long_api_key_string
+
+heroku ps:scale web=1
+```
+
+On your code:
+
+```javascript
+
+this.importio.init({¬                                                   
+             "auth": "http://adjective-noum-9999.herokuapp.com/",¬                     
+             "host": "import.io"¬                                                
+});¬
+```
+
+## References
+
+This doc explains how to use signed queries:
+http://docs.import.io/35/signedserver.html
+
+## A word of caution
+
+Please be warned that _you_ are the sole responsible for securing your 
+keys and you should not blame anyone but you if something bad happen with them.
+
+Keys being stolen from heroku via 0-day exploits or accidentaly being commited on
+a public GitHub repo are examples of that.
---
+++ index.js
@@ -0,0 +1,75 @@
+var connect = require('connect');
+var morgan  = require('morgan');
+var http 	= require('http');
+var crypto 	= require('crypto');
+var compression = require('compression');
+
+// Your keys.
+var userGuid = process.env.IMPORTIO_USERGUID;
+var apiKey   = process.env.IMPORTIO_APIKEY;
+var orgGuid  = "00000000-0000-0000-0000-000000000000";
+
+var rawKey   = new Buffer(apiKey, 'base64');
+
+// Creates a Hmac SHA1 based on a key.
+function sign (query){
+
+	var hmac = crypto.createHmac('sha1', rawKey);
+
+	// console.log(query);
+	// console.log(JSON.stringify(query));
+
+	 // 24 hours expiration. set to "null" for no expiry.
+	var expiry = (Date.now() + (60*60*24)) * 1000;
+	var check = query + ':' + userGuid + ':' + expiry;
+
+	// console.log(check);
+	hmac.update(check)
+	var digest = hmac.digest('base64');	
+	// console.log(digest);
+
+	var signedQuery = {
+		"queryJson": query,
+		"expiresAt": expiry,
+		"userGuid": userGuid,
+		"orgGuid": orgGuid,
+		"digest": digest
+	};
+	// console.log(JSON.stringify(signedQuery));
+	return JSON.stringify(signedQuery);
+}
+
+var app = connect();
+app.use(morgan());
+app.use(compression());
+
+// simple function to get raw body data.
+app.use(function(req, res, next){
+   var data = "";
+   req.on('data', function(chunk){ data += chunk})
+   req.on('end', function(){
+      req.rawBody = data;
+      next();
+   })
+})
+
+// Main route
+app.use(function(req, res){
+	var origin = req.headers['origin'];
+	// console.log(JSON.stringify(req.headers));
+
+	res.setHeader('Content-Type', 'application/json');
+
+	if(origin){
+		res.setHeader("Access-Control-Allow-Credentials", "true");
+		res.setHeader("Access-Control-Allow-Origin", origin);
+	}
+
+	res.end(sign(req.rawBody));
+})
+
+var port = Number(process.env.PORT || 5000);
+
+http.createServer(app).listen(port, function(){
+	console.log("Server started on port " + port);
+});
